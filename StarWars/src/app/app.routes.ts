a76bfd0deeae4ae9cf97a65b595122eae79285cd
David Espin <david.espin.morales@everis.com>
2019-04-08 11:08:47 +0200

    routing

---
+++ StarWars/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '**', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FeatureRoutingModule {}
